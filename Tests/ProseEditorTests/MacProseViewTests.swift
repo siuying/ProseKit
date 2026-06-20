60fa02cc209732b44696f2160b16e4f1531843e4
Francis Chong <francis@ignition.hk>
2026-06-20 18:20:09 +0100

    feat: add macOS caret selection layer

--- Tests/ProseEditorTests/MacProseViewTests.swift
+++ Tests/ProseEditorTests/MacProseViewTests.swift
@@ -19,7 +19,47 @@ final class MacProseViewTests: XCTestCase {
         XCTAssertFalse(canvas.wantsLayer)
         XCTAssertEqual(canvas.frame.width, 320)
         XCTAssertGreaterThan(canvas.frame.height, 0)
+        XCTAssertTrue(view.canvasView.superview === canvas)
+        XCTAssertTrue(view.selectionLayer.superview === canvas)
+        XCTAssertTrue(canvas.subviews.first === view.canvasView)
+        XCTAssertTrue(canvas.subviews.last === view.selectionLayer)
         XCTAssertTrue(view.hasVerticalScroller)
+    }
+
+    func testClickingInMacViewPlacesACaretThroughSharedGeometry() throws {
+        let view = ProseView(document: Document(.doc([
+            .paragraph([.text("hello mac")]),
+        ])))
+        view.frame = CGRect(x: 0, y: 0, width: 320, height: 120)
+        view.layoutSubtreeIfNeeded()
+        let click = CGPoint(x: 74, y: 18)
+        let expectedPosition = view.core.closestPosition(to: click)
+
+        view.placeCaret(atContentPoint: click)
+
+        XCTAssertEqual(view.core.selection, TextSelection(anchor: expectedPosition, head: expectedPosition))
+        XCTAssertEqual(view.selectionLayer.selection, view.core.selection)
+        XCTAssertEqual(view.selectionLayer.caretRect, view.core.caretRect(for: expectedPosition))
+    }
+
+    func testMacCaretVisibilityTracksFirstResponderState() throws {
+        let view = ProseView(document: Document(.doc([
+            .paragraph([.text("hello mac")]),
+        ])))
+        view.frame = CGRect(x: 0, y: 0, width: 320, height: 120)
+        view.layoutSubtreeIfNeeded()
+
+        view.placeCaret(atContentPoint: CGPoint(x: 42, y: 18))
+        XCTAssertTrue(view.selectionLayer.drawsCaret)
+        XCTAssertNotNil(view.selectionLayer.blinkTimer)
+
+        XCTAssertTrue(view.resignFirstResponder())
+        XCTAssertFalse(view.selectionLayer.drawsCaret)
+        XCTAssertNil(view.selectionLayer.blinkTimer)
+
+        XCTAssertTrue(view.becomeFirstResponder())
+        XCTAssertTrue(view.selectionLayer.drawsCaret)
+        XCTAssertNotNil(view.selectionLayer.blinkTimer)
     }
 
     func testMacHighlightPaletteResolvesAcrossAppearances() throws {
